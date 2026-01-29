73fb006bf026a2e3d2e785c7d27286fc247f5926
QSY <3234946025@qq.com>
2026-01-29 21:38:36 +0800

    补充了优秀模型的脚本

---
+++ scripts/long_term_forecast_noise/ETT_script/Super_DP_RTM_ETTh1_dropout.sh
@@ -0,0 +1,125 @@
+export CUDA_VISIBLE_DEVICES=0,1,2
+
+model_name=Super_DP_RTM
+
+seq_len=96
+e_layers=2
+down_sampling_layers=3
+down_sampling_window=2
+learning_rate=0.01
+d_model=16
+d_ff=32
+train_epochs=10
+patience=10
+batch_size=16
+data_name=ETTh1_noise_dropout
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path  ./dataset/ETT-small/\
+  --data_path $data_name.csv \
+  --model_id $data_name'_'$seq_len'_'96 \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 0 \
+  --pred_len 96 \
+  --e_layers $e_layers \
+  --enc_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --batch_size 128 \
+  --down_sampling_layers $down_sampling_layers \
+  --down_sampling_method avg \
+  --down_sampling_window $down_sampling_window
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path $data_name.csv \
+  --model_id $data_name'_'$seq_len'_'192 \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 0 \
+  --pred_len 192 \
+  --e_layers $e_layers \
+  --enc_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --batch_size 128 \
+  --down_sampling_layers $down_sampling_layers \
+  --down_sampling_method avg \
+  --down_sampling_window $down_sampling_window
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path $data_name.csv \
+  --model_id $data_name'_'$seq_len'_'336 \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 0 \
+  --pred_len 336 \
+  --e_layers $e_layers \
+  --enc_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --batch_size 128 \
+  --down_sampling_layers $down_sampling_layers \
+  --down_sampling_method avg \
+  --down_sampling_window $down_sampling_window
+
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path $data_name.csv \
+  --model_id $data_name'_'$seq_len'_'720 \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 0 \
+  --pred_len 720 \
+  --e_layers $e_layers \
+  --enc_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --learning_rate $learning_rate \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --batch_size 128 \
+  --down_sampling_layers $down_sampling_layers \
+  --down_sampling_method avg \
+  --down_sampling_window $down_sampling_window
