e624f66c045cb1e2d3fe356ea1f51a245a05b4ec
QSY <3234946025@qq.com>
2026-01-31 17:24:35 +0800

    删除了os,修bug

--- exp/exp_long_term_forecasting.py
+++ exp/exp_long_term_forecasting.py
@@ -266,7 +266,6 @@ class Exp_Long_Term_Forecast(Exp_Basic):
         np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
         np.save(folder_path + 'pred.npy', preds)
         np.save(folder_path + 'true.npy', trues)
-        import os
         best_model_path = os.path.join(self.args.checkpoints, setting, 'checkpoint.pth')
         try:
             if os.path.exists(best_model_path):
