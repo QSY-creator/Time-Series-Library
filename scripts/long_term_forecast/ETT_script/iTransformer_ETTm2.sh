1c009eaa548a36e55d8c9182718b2d6621d1e3ae
QSY <3234946025@qq.com>
2026-01-31 17:34:43 +0800

    补充了ETTm2的脚本

---
+++ scripts/long_term_forecast/ETT_script/iTransformer_ETTm2.sh
@@ -0,0 +1,95 @@
+export CUDA_VISIBLE_DEVICES=0,1,2
+
+model_name=iTransformer
+data_name=ETTm2
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path $data_name.csv \
+  --model_id $data_name'_'96'_'96 \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --d_model 128 \
+  --d_ff 128 \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path $data_name.csv \
+  --model_id $data_name'_'96'_'192 \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --d_model 128 \
+  --d_ff 128 \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path $data_name.csv \
+  --model_id $data_name'_'96'_'336 \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --d_model 128 \
+  --d_ff 128 \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path $data_name.csv \
+  --model_id $data_name'_'96'_'720 \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --d_model 128 \
+  --d_ff 128 \
+  --itr 1
