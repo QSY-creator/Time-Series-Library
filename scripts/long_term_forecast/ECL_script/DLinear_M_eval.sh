ddb2f48f826e3b32918fb44ed67655faed5a8303
QSY <3234946025@qq.com>
2026-06-26 10:18:28 +0800

    创建了评估脚本，以补充未收集到的指标

---
+++ scripts/long_term_forecast/ECL_script/DLinear_M_eval.sh
@@ -0,0 +1,28 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=DLinear
+
+for pred_len in 96 192 336 720
+do
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 0 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_96_${pred_len} \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len ${pred_len} \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --des 'M_eval' \
+  --itr 1 \
+  --output_base ./exp_outputs/ECL_DLinear/
+done
